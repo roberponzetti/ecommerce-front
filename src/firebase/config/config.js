79adcf3ae7eb1e771579aca26b37d3220a4a99d6
Nicolas N Fernandez <nicolasnahuel.pr@gmail.com>
2022-09-26 09:28:49 -0300

    chore: added enviroment variables for firebase

--- src/firebase/config/config.js
+++ src/firebase/config/config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBz_j60XtHD57RIvMVueoU79mE2aMuV3HM",
-  authDomain: "ecommerce-grupo1.firebaseapp.com",
-  projectId: "ecommerce-grupo1",
-  storageBucket: "ecommerce-grupo1.appspot.com",
-  messagingSenderId: "429174702861",
-  appId: "1:429174702861:web:1dcfd9648589af594de644"
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN_FIREBASE,
+  projectId: process.env.REACT_APP_PROJECT_ID_FIREBASE,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET_FIREBASE,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_KEY_FIREBASE,
+  appId: process.env.REACT_APP_APP_ID_FIREBASE,
 };
