5ca02bb3b13eff20f537226b3e900714e3571912
roberponzetti <51685639+roberponzetti@users.noreply.github.com>
2022-10-28 17:34:32 -0300

    50% nav

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { openCartNavbar, selectCart } from "../../redux/state/cart";
 import { useDispatch, useSelector } from "react-redux";
@@ -19,11 +19,16 @@ const NavbarItem = () => {
   const dispatch = useDispatch()
   const { cart, openCart } = useSelector(selectCart);
   const { user } = useSelector(selectAuth);
+  const navigate = useNavigate();
 
   const totalQuantity = cart.reduce((prevValue, currentValue) => prevValue + currentValue.quantity, 0);
 
   const handleLogout = () => {
     dispatch(logoutUser());
+  }
+
+  const handleLogin = () => {
+    navigate("/login");
   }
 
   const handleBoxToggle = () => {
@@ -35,7 +40,7 @@ const NavbarItem = () => {
   }
 
   return (
-    <Navbar variant="dark" className={style.navBar}>
+    <Navbar variant="dark" className={style.navBar} expand="sm">
       <Container>
         {openCart && <div className={style.overlay}></div>}
         <div className="d-flex justify-content-center align-items-center">
@@ -43,41 +48,70 @@ const NavbarItem = () => {
             <AiOutlineHome className={globalStyle.mr_1} />
             Inicio
           </Link>
-          {user ?
-            <Link className={clx(style.outline, style.item_nav)} onClick={handleLogout}>
-              <RiLogoutCircleLine className={globalStyle.mr_1} />
-              Cerrar sesión
-            </Link>
-            :
-            <Link className={clx(style.outline, style.item_nav)} to="/login">
-              <BiLogInCircle className={globalStyle.mr_1} />
-              Iniciar sesión
-            </Link>
-          }
-        </div>
-        <div className="d-flex align-items-center text-white ">
-          <div className="me-5 d-flex align-items-center justify-content-start">
-            {user &&
-              <>
-                <FaUser className='me-3' size={20} />
-                <div>
-                  <p className={clx(style.info_user, "text-white text-start m-0 p-0 fw-bold")} >¡HOLA!</p>
-                  <p className={clx(style.info_user, "text-white m-0 p-0 font-weight-light")}>{user?.displayName ? user.displayName : user?.email}</p>
-                </div>
-              </>
-            }
+          <div className="d-flex align-items-center text-white ">
+            {/* {user ? ( */}
+            {/* <> */}
+            <FaUser className='me-3' size={20} />
+            <div>
+              <NavDropdown
+                title={user?.displayName ? user.displayName : user?.email}
+              >
+                {user ?
+                  <NavDropdown.Item className="p-0">
+                    <Button className={clx(style.outline, style.item_nav, "text-dark", "border-0")} onClick={handleLogout} variant="outline-light">
+                      <RiLogoutCircleLine className={globalStyle.mr_1} />
+                      Cerrar sesión
+                    </Button>
+                  </NavDropdown.Item>
+                  :
+                  <NavDropdown.Item>
+                    <Button className={clx(style.outline, style.item_nav, "text-dark", "border-0")} onClick={handleLogin} variant="outline-light">
+                      <BiLogInCircle className={globalStyle.mr_1} />
+                      Iniciar sesión
+                    </Button>
+                  </NavDropdown.Item>
+                }
+              </NavDropdown>
+              {/* <p className={clx(style.info_user, "text-white text-start m-0 p-0 fw-bold")} >¡HOLA!</p>
+                  <p className={clx(style.info_user, "text-white m-0 p-0 font-weight-light")}>{user?.displayName ? user.displayName : user?.email}</p> */}
+            </div>
+            {/* </> */}
+            {/* ) : (
+
+            )} */}
           </div>
-          <Link className={clx(style.item_nav)} onMouseOver={handleBoxToggle} to="/cart">
-            <AiOutlineShoppingCart className={globalStyle.mr_1} size={25} />
-            <span className={clx(style.badge, "rounded-pill", "badge-notification")}>
-              {totalQuantity}
-            </span>
-          </Link>
-          <span>{priceFormatted(totalPrice(cart))}</span>
+        </div>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+
+        <div className="d-flex align-items-center text-white ">
+          <Navbar.Collapse id="navbarScroll">
+            {/* <div className="me-5 d-flex align-items-center justify-content-start"> */}
+            {/* {user ?
+                <Link className={clx(style.outline, style.item_nav)} onClick={handleLogout}>
+                  <RiLogoutCircleLine className={globalStyle.mr_1} />
+                  Cerrar sesión
+                </Link>
+                :
+                <Link className={clx(style.outline, style.item_nav)} to="/login">
+                  <BiLogInCircle className={globalStyle.mr_1} />
+                  Iniciar sesión
+                </Link>
+              } */}
+
+            {/* </div> */}
+            <Link className={clx(style.item_nav)} onMouseOver={handleBoxToggle} to="/cart">
+              <AiOutlineShoppingCart className={globalStyle.mr_1} size={25} />
+              <span className={clx(style.badge, "rounded-pill", "badge-notification")}>
+                {totalQuantity}
+              </span>
+            </Link>
+            <span>{priceFormatted(totalPrice(cart))}</span>
+          </Navbar.Collapse>
         </div>
         {openCart && <CartNavbar handleBoxLeaveToggle={handleBoxLeaveToggle} />}
+
       </Container>
-    </Navbar>
+    </Navbar >
   );
 };
 
