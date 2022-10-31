2441da82a6824acf0a347cce11c8628aea41be2f
roberponzetti <51685639+roberponzetti@users.noreply.github.com>
2022-10-31 17:13:14 -0300

    idleTimer

--- src/routes/RoutesComponent.jsx
+++ src/routes/RoutesComponent.jsx
@@ -6,9 +6,34 @@ import { routes } from "../pages/routes";
 import { Layout } from "../components/Layout";
 import { useProduct } from "../hooks/useProduct";
 import { useAuth } from "../hooks/useAuth";
+import Swal from "sweetalert2";
+import { useIdleTimer } from "react-idle-timer";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutUser, selectAuth } from "../redux/state/auth";
 
 const RoutesComponent = () => {
+
+  const { user } = useSelector(selectAuth);
+  const dispatch = useDispatch();
+
+  const onIdle = () => {
+    if (user) {
+      Swal.fire({
+        title: 'Su sesión ha expirado. Vuelva a iniciar sesión.', confirmButtonColor: '#facea8', icon: 'warning', customClass: {
+          title: 'title_sweet_alert',
+        }, allowOutsideClick: false
+      });
+      dispatch(logoutUser());
+    }
+  }
+
+  useIdleTimer({
+    onIdle,
+    timeout: 10000
+  })
+
   const { isLoading } = useProduct();
+
   useAuth();
 
   if (isLoading) return <Loading />
